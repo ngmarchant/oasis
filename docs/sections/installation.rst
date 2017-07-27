d57c99b2b57af57c1676f4f5569a0f7fe40ea4df
ngmarchant <ngmarchant@gmail.com>
2017-07-27 12:47:00 +1000

    Updated experiments code to support multiple classifiers

--- docs/sections/installation.rst
+++ docs/sections/installation.rst
@@ -7,11 +7,11 @@ supported.
 
 Installation from source
 ------------------------
-Currently the package must be installed from source, although in future it may
-be available on PyPI.
+You can get the latest stable release from `PyPI <https://pypi.python.org/pypi>`_ by running::
 
-To clone the source from `GitHub <https://www.github.com/ngmarchant/oasis>`_,
-execute the following command::
+    $ pip3 install oasis
+    
+Alternatively, you can experiment with the latest development code by cloning the source from `GitHub <https://www.github.com/ngmarchant/oasis>`_::
 
     $ git clone https://github.com/ngmarchant/oasis.git
 
@@ -26,7 +26,7 @@ OASIS depends on the following Python packages:
 
 * numpy
 * scipy
-* sklearn (to use the experimental stratification method based on features)
+* sklearn (to use the experimental stratification method based on K-means clustering)
 * tables (to use :class:`Data` container)
 
 These dependencies should be automatically resolved during installation.
--- oasis/experiments.py
+++ oasis/experiments.py
@@ -4,29 +4,27 @@ import time
 import logging
 import warnings
 
-def repeat_expt(sampling_obj, num_expts, num_labels, output_file = None):
+def repeat_expt(smplr, n_expts, n_labels, output_file = None):
     """
-    Input
-    -----
-    sampling_obj
+    Parameters
+    ----------
+    smplr : sub-class of PassiveSampler
+        sampler must have a sample_distinct method, reset method and ...
 
-    num_expts        number of expts to run
+    n_expts : int
+        number of expts to run
 
-    num_labels       number of labels to query from the oracle in each expt
-
-
-    sampling_obj must have:
-    sample_until method
-    reset method
-    F property
+    n_labels : int
+        number of labels to query from the oracle in each expt
     """
 
     FILTERS = tables.Filters(complib='zlib', complevel=5)
 
-    max_iter = sampling_obj._max_iter
-    if max_iter < num_labels:
+    max_iter = smplr._max_iter
+    n_class = smplr._n_class
+    if max_iter < n_labels:
         raise ValueError("Cannot query {} labels. {} supports only {} \
-            iterations".format(num_labels, sampling_obj.__name__, max_iter))
+            iterations".format(n_labels, smplr.__name__, max_iter))
 
     if output_file is None:
         # Use current date/time as filename
@@ -38,45 +36,43 @@ def repeat_expt(sampling_obj, num_expts, num_labels, output_file = None):
     bool_atom = tables.BoolAtom()
     int_atom = tables.Int64Atom()
 
-    array_F = f.create_earray(f.root, 'F_measure', float_atom, (0, num_labels))
-    array_s = f.create_carray(f.root, 'n_iterations', int_atom, (num_expts, 1))
-    array_t = f.create_carray(f.root, 'CPU_time', float_atom, (num_expts, 1))
+    array_F = f.create_carray(f.root, 'F_measure', float_atom, (n_expts, n_labels, n_class))
+    array_s = f.create_carray(f.root, 'n_iterations', int_atom, (n_expts, 1))
+    array_t = f.create_carray(f.root, 'CPU_time', float_atom, (n_expts, 1))
 
-    logging.info("Starting {} experiments".format(num_expts))
-    for idx in range(num_expts):
-        if idx%100 == 0:
-            logging.info("Completed {} of {} experiments".format(idx, num_expts))
+    logging.info("Starting {} experiments".format(n_expts))
+    for i in range(n_expts):
+        if i%np.ceil(n_labels/10).astype(int) == 0:
+            logging.info("Completed {} of {} experiments".format(i, n_expts))
         ti = time.process_time()
-        sampling_obj.reset()
-        sampling_obj.sample_distinct(num_labels)
+        smplr.reset()
+        smplr.sample_distinct(n_labels)
         tf = time.process_time()
-        if hasattr(sampling_obj, 'queried_oracle_'):
-            array_F.append([sampling_obj.estimate_[sampling_obj.queried_oracle_]])
+        if hasattr(smplr, 'queried_oracle_'):
+            array_F[i,:,:] = smplr.estimate_[smplr.queried_oracle_]
         else:
-            array_F.append([sampling_obj.estimate_])
-        array_s[idx] = sampling_obj.t_
-        array_t[idx] = tf - ti
+            array_F[i,:,:] = smplr.estimate_
+        array_s[i] = smplr.t_
+        array_t[i] = tf - ti
     f.close()
 
     logging.info("Completed all experiments")
 
-def process_expt(h5_path, verbose = False, inmemory = True, ignorenan = False):
+def process_expt(h5_path, inmemory = True, ignorenan = False):
     """
     Assumes h5 file has table called `F_measure`
 
-    Input
-    -----
+    Parameters
+    ----------
     h5_path : string
+        path to HDF file containing the experimental data. The file is expected
+        to have been generated from the `repeat_expt` function.
 
-    verbose : boolean
+    inmemory : bool
+        whether to process the experiments in memory
 
-    inmemory : boolean
-
-    Output
-    ------
-    F_mean : float numpy.ndarray
-
-    F_var : float numpy.ndarray
+    ignorenan : bool
+        whether to ignore NaNs when computing the mean and variance
     """
 
     logging.info("Reading file at {}".format(h5_path))
@@ -84,8 +80,7 @@ def process_expt(h5_path, verbose = False, inmemory = True, ignorenan = False):
 
     F = h5_file.root.F_measure
 
-    n_labels = F.shape[1]
-    n_expt = F.shape[0]
+    n_expt, n_labels, n_class = F.shape
     mean_n_iterations = np.sum(h5_file.root.n_iterations)/n_expt
 
     if hasattr(h5_file.root, 'CPU_time'):
@@ -97,34 +92,34 @@ def process_expt(h5_path, verbose = False, inmemory = True, ignorenan = False):
         var_CPU_time = None
         mean_CPU_time_per_iteration = None
 
-    F_mean = np.empty(n_labels, dtype='float')
-    F_var = np.empty(n_labels, dtype='float')
-    F_stderr = np.empty(n_labels, dtype='float')
+    F_mean = np.empty([n_labels, n_class], dtype='float')
+    F_var = np.empty([n_labels, n_class], dtype='float')
+    F_stderr = np.empty([n_labels, n_class], dtype='float')
     n_sample = np.empty(n_labels, dtype='int')
     if inmemory:
-        F_mem = F[:,:]
+        F_mem = F[:,:,:]
 
     logging.info("Beginning processing".format())
     for t in range(n_labels):
 
-        if t%1000 == 0:
+        if t%np.ceil(n_labels/10).astype(int) == 0:
             logging.info("Processed {} of {} experiments".format(t, n_labels))
         if inmemory:
-            temp = F_mem[:,t]
+            temp = F_mem[:,t,:]
         else:
-            temp = F[:,t]
+            temp = F[:,t,:]
         if ignorenan:
             n_sample[t] = np.sum(~np.isnan(temp))
             # Expect to see RuntimeWarnings if array contains all NaNs
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", category=RuntimeWarning)
-                F_mean[t] = np.nanmean(temp)
-                F_var[t] = np.nanvar(temp)
+                F_mean[t] = np.nanmean(temp, axis=0)
+                F_var[t] = np.nanvar(temp, axis=0)
                 F_stderr[t] = np.sqrt(F_var[t]/n_sample[t])
         else:
             n_sample[t] = len(temp)
-            F_mean[t] = np.mean(temp)
-            F_var[t] = np.var(temp)
+            F_mean[t] = np.mean(temp, axis=0)
+            F_var[t] = np.var(temp, axis=0)
             F_stderr[t] = np.sqrt(F_var[t]/n_sample[t])
 
     logging.info("Processing complete".format())
@@ -176,17 +171,20 @@ class Data:
             self.num_fts = h5_file.root.features.shape[1]
         if hasattr(h5_file.root, "labels"):
             self.labels = h5_file.root.labels[:]
+            self.num_items = len(self.labels)
         if hasattr(h5_file.root, "scores"):
             self.scores = h5_file.root.scores[:]
+            self.num_items = len(self.scores)
         if hasattr(h5_file.root, "probs"):
             self.probs = h5_file.root.probs[:]
+            self.num_items = len(self.probs)
         if hasattr(h5_file.root, "preds"):
             self.preds = h5_file.root.preds[:]
             self.num_items = len(self.preds)
 
         h5_file.close()
 
-    def scores_to_preds(self, threshold = 0, use_probs = True):
+    def scores_to_preds(self, threshold, use_probs = True):
         """
         use_probs : boolean, default True
             if True, use probabilities for predictions, else use scores.
@@ -225,7 +223,7 @@ class Data:
 
         if self.preds is None:
             raise DataError("Predictions not available. Please run "
-                            "`make_predictions` before calculating confusion "
+                            "`scores_to_preds` before calculating confusion "
                             "matrix")
 
         self.TP = np.sum(np.logical_and(self.preds == 1, self.labels == 1))
@@ -245,7 +243,6 @@ class Data:
         """
         Evaluate precision, recall and balanced F-measure
         """
-
         try:
             self.calc_confusion_matrix(printout = False)
         except DataError as e:
